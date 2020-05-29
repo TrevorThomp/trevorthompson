61ff2fb765116c0dadf58ec33d72fa304fdea48e
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-05-29 12:02:24 -0700

    modularized navLink anchors

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import { navLinks } from "../config";
 import { theme } from "../styles";
-const { colors } = theme;
+import { Link } from "gatsby";
+const { colors, fontSizes } = theme;
 
 const StyledNav = styled.nav`
 	display: flex;
@@ -10,27 +12,39 @@ const StyledNav = styled.nav`
 
 const StyledList = styled.ol`
 	display: flex;
-	justify-content: space-evenly;
+	justify-content: space-between;
 	align-items: center;
 	margin: 0;
 	list-style: none;
 	width: 300px;
+	font-size: ${fontSizes.md};
 `;
 
 const StyledListItem = styled.li`
+	margin: 0 10px;
+
 	&:before {
 		counter-increment: item 1;
 		content: "0" counter(item) ".";
+		color: ${colors.lightGreen};
 	}
+`;
+
+const StyledListLink = styled(Link)`
+	padding: 10px;
+	text-decoration: none;
 `;
 
 const Nav = () => {
 	return (
 		<StyledNav>
 			<StyledList>
-				<StyledListItem>Test</StyledListItem>
-				<StyledListItem>Test</StyledListItem>
-				<StyledListItem>Test</StyledListItem>
+				{navLinks &&
+					navLinks.map(({ name, url }, i) => (
+						<StyledListItem key={i}>
+							<StyledListLink to={url}>{name}</StyledListLink>
+						</StyledListItem>
+					))}
 			</StyledList>
 		</StyledNav>
 	);
---
+++ src/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	navLinks: [
+		{
+			name: "About",
+			url: "/#about",
+		},
+		{
+			name: "Experience",
+			url: "/#jobs",
+		},
+		{
+			name: "Work",
+			url: "/#projects",
+		},
+		{
+			name: "Contact",
+			url: "/#contact",
+		},
+	],
+};
