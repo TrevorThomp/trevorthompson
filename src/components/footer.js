5630480701f7ec33a66737aa9d727e04bdb4086f
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-06-04 11:29:43 -0700

    add bottom border

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 import { theme } from "../styles";
-const { font, fontSizes } = theme;
+const { font, fontSizes, secondColors } = theme;
 
 const StyledFooter = styled.footer`
+	position: relative;
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	width: 100%;
 	font-family: ${font.roboto};
 	font-size: ${fontSizes.md};
+
+	&:after {
+		content: "";
+		height: 1rem;
+		width: 100%;
+		position: absolute;
+		// bottom: -1px;
+		bottom: 0px;
+		left: 0px;
+		background-color: ${secondColors.teal};
+		background-image: linear-gradient(
+			315deg,
+			${secondColors.smokyBlack} 1%,
+			${secondColors.teal} 100%
+		);
+	}
 `;
 
 const Footer = () => {
