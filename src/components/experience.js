5db423992e5574ea0526c8c61316976b1a92f5a8
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-08-06 21:03:19 -0700

    updated resume

--- src/components/experience.js
+++ src/components/experience.js
@@ -256,6 +256,7 @@ const TitleDetailBlockWrapper = styled.div`
 `;
 
 const Experience = ({ data }) => {
+	console.log(data);
 	const [index, setIndex] = useState(0);
 	const revealContainer = useRef(null);
 	useEffect(() => scrollReveal.reveal(revealContainer.current, srConfig()), []);
